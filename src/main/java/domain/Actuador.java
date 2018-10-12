6a25ffd1f43e0c9480e8a110e1e7bfbc2f6e5fec
gaston paz <gaston_mpaz@hotmail.com>
2018-10-12 20:09:07 -0300

    persistencia actuador y sensor

--- src/main/java/domain/Actuador.java
+++ src/main/java/domain/Actuador.java
@@ -2,7 +2,16 @@ package domain;
 
 import java.util.HashMap;
 
-public class Actuador {
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "actuador")
+public class Actuador extends EntidadPersistente {
+	@ManyToOne
+	@JoinColumn(name = "dispositivo_inteligente_id", referencedColumnName = "id")
 	private DispositivoInteligente dispositivo;
 	private HashMap<Acciones, Command> listaComandos;
 
--- src/main/java/domain/Sensor.java
+++ src/main/java/domain/Sensor.java
@@ -2,7 +2,12 @@ package domain;
 
 import java.util.ArrayList;
 
-public class Sensor implements Subject {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sensor")
+public class Sensor extends EntidadPersistente implements Subject {
 	private ArrayList<Observer> observers;
 	private Medicion medicion;
 
